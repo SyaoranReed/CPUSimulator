057b265fc275f7fde3b959901ac3d6f7ca002d06
SyaoranReed <capitan_ro@hotmail.com>
2017-12-10 14:35:30 -0300

    Se agrego el metodo para cargar las instrucciones a la memoria

--- src/cpuSimulator/Main.java
+++ src/cpuSimulator/Main.java
@@ -1,5 +1,12 @@
 package cpuSimulator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 public class Main {
 
 	
@@ -16,6 +23,25 @@ public class Main {
 		CPU cpu = new CPU(instructionMemory, dataMemory);
 		cpu.startProgram();
 
+	}
+	
+	public void loadInstruction(String pathname, Memory instructionMemory) throws IOException {
+		File file = new File(pathname);
+		if (!file.exists()) {
+			throw new FileNotFoundException("Don't exist" + pathname);
+		}
+		FileReader fileReader = new FileReader(file);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		String line;
+		try {
+			int i = 0;
+			while((line = bufferedReader.readLine()) != null) {
+				instructionMemory.write(line, Integer.toBinaryString(i++));
+			}
+		} catch (IOException e) {
+			
+			throw new IOException("I/O error");
+		}
 	}
 
 }
